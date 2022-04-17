67c191b1fc779c7b064fe61ecfe685e8dc0b73ab
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-04-17 22:08:08 +0530

    add navbar, header
    remove demo components

---
+++ components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <div class="page-header">
+      <div
+        class="position-absolut fixed-top position-fixe me-auto w-100 w-lg-50 h-100 z-index d-non d-sm-non d-md-bloc"
+        style="
+          background-image: url(https://images.unsplash.com/photo-1632067117908-826d2531dba9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80);
+          background-size: cover;
+          z-index: -1;
+        "
+        loading="lazy"
+      ></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5"></div>
+          <div
+            class="col-lg-7 d-flex justify-content-center align-items-center"
+            style="height: 70vh"
+          >
+            <div
+              class="card card-body blur d-flex justify-content-center shadow-lg p-5 mt-5"
+            >
+              <h1>Hardeep Kumar</h1>
+              <p class="lead">I develop: Full Stack, Backend, APIs</p>
+              <p class="lead">
+                Technologies I use: Django, Django Rest Framework, Nuxt, Vue,
+                Bootstrap
+              </p>
+              <!-- <div class="buttons">
+                <button type="button" class="btn bg-gradient-primary mt-4">
+                  Get Started
+                </button>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style></style>
---
+++ components/NavBar..vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Navbar -->
+  <nav
+    class="navbar navbar-expand-lg bg-transparent navbar-dark shadow-0"
+    style="z-index: 0"
+  >
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Navbar brand -->
+      <a class="navbar-brand rounded-circle bg-dark text-light p-1" href="#"
+        >HK</a
+      >
+
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler bg-dark"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <fa :icon="['fas', 'bars']" />
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <!-- Link -->
+          <li class="nav-item my-md-2">
+            <a class="nav-link d-inline-block bg-dark px-md-2 py-md-1 text-white rounded-9 bg-gradient" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
