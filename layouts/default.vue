c90bf2db3430be07a6d37b4d4a166d9d8d4b834f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-04-18 18:25:21 +0530

    update page styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="vh-100">
+    <div class="bg-gradient-gray vw-100 vh-100 position-fixed z-index-n5"></div>
+    <div class="bg-pattern vw-100 vh-100 position-fixed z-index-n4"></div>
     <NavBar />
     <Nuxt />
   </div>
