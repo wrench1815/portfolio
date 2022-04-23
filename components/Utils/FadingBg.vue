9ce5bb57120d27158f403e9c4f936660c4616274
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-04-23 23:01:11 +0530

    add fading effect component

---
+++ components/Utils/FadingBg.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="directionClass"></div>
+</template>
+
+<script>
+export default {
+  name: 'FadingBg',
+
+  props: {
+    direction: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      directionClass: 'fading-bg-down',
+    }
+  },
+
+  created() {
+    if (this.direction == 'down') {
+      this.directionClass = 'fading-bg-down'
+    } else if (this.direction == 'up') {
+      this.directionClass = 'fading-bg-up'
+    }
+  },
+}
+</script>
+
+<style scoped>
+.fading-bg-down {
+  height: 20vh;
+  background: linear-gradient(#ffff 2%, transparent);
+}
+
+.fading-bg-up {
+  height: 20vh;
+  background: linear-gradient(transparent, #ffff 98%);
+}
+</style>
