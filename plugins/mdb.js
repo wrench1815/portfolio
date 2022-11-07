1604be6e356691faa0d887552c2613ff1d0217e6
wrench1815 <hardeepkumar1815@gmail.com>
2022-11-07 17:11:24 +0530

    cleanup

--- plugins/mdb.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import * as mdb from 'mdb-ui-kit'
-
-import 'mdb-ui-kit/src/scss/mdb.free.scss'
-import '~/assets/css/styles.css'
-import '~/assets/css/overides.css'
-import '~/assets/css/transitions.css'
-
-Vue.use(mdb)
