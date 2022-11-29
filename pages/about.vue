d70b4277a84189de6c309808453d072855578be3
wrench1815 <hardeepkumar1815@gmail.com>
2022-11-29 11:49:23 +0530

    📦 NEW: add abour page

--- pages/about.vue
+++ pages/about.vue
@@ -1,54 +1,14 @@
 <script lang="ts" setup>
-import Typed from 'typed.js';
-
-const typing = ref('');
-const hideStopBtn = ref(false);
-const hideTyping = ref(false);
-
-const currentYear = () => {
-  return new Date().getFullYear();
-};
-
-function stop() {
-  hideTyping.value = true;
-  hideStopBtn.value = true;
-}
-
-onMounted(() => {
-  let str = document.querySelector('#typeBase')?.innerHTML as string;
-  new Typed(typing.value, {
-    strings: [str],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: false,
-    showCursor: false,
-    onComplete: () => {
-      // hideStopBtn.value = true;
-    },
-  });
-});
+const myselfImage = ref(
+  'https://res.cloudinary.com/dnzbu6wqv/image/upload/v1669702340/myself/94FKLoemnSP7ikhmOunf--3--2fao5_4x_at5csh.jpg'
+);
 </script>
 
 <template>
-  <div class="absolute top-[65px] right-5" v-if="!hideStopBtn">
-    <button class="btn btn-circle" @click="stop">
-      <v-icon name="ri-stop-fill" scale="1.2" />
-    </button>
-  </div>
-
   <main class="container mx-auto px-4">
-    <section
-      class="prose prose-lg max-w-none"
-      ref="typing"
-      v-if="!hideTyping"
-    ></section>
-  </main>
-
-  <main class="container mx-auto px-4" :class="{ hidden: !hideTyping }">
-    <section class="prose prose-lg max-w-none" id="typeBase">
+    <section class="prose max-w-none">
       <div class="flex gap-2">
-        <h1 class="mb-0">Hi there!</h1>
+        <h1 class="mb-12">Hii!!!</h1>
         <v-icon
           name="md-wavinghand"
           scale="2.5"
@@ -56,156 +16,160 @@ onMounted(() => {
           class="text-primary"
         />
       </div>
+      <div
+        class="not-prose flex flex-col justify-center items-center relative gap-2"
+      >
+        <div
+          class="absolute top-0 bottom-0 right-0 left-0 mx-auto bg-secondary max-w-full h-auto aspect-square object-cover -rotate-3 md:max-w-sm"
+        ></div>
+        <div
+          class="absolute top-0 bottom-0 right-0 left-0 mx-auto bg-primary max-w-full h-auto aspect-square object-cover rotate- md:max-w-sm"
+        ></div>
+        <img
+          class="max-w-full h-auto aspect-square object-cover rotate-3 shadow-lg md:max-w-sm"
+          :src="myselfImage"
+          alt="Hardeep Kumar AI genarted art"
+        />
 
-      <h2>This is Hardeep Kumar. Nice to meet you.</h2>
-
-      <p class="font-serif text-2xl">
-        You might be wondering, what this site is for? Well It is a Portfolio
-        Site of mine with a Blog integrated into it. This Site is supposed to
-        showcase my projects and some of the Blogs that i wrote or might write
-        in the future.
-      </p>
-
-      <h4 class="text-error">Now, Let me guide you through the Site!!</h4>
-
-      <p class="font-serif text-2xl">The Stack of this site is a follow:</p>
-      <ul class="font-serif text-2xl">
-        <li class="text-warning">
-          <a
-            class="text-warning inline-flex items-center gap-1 no-underline my-0"
-            href="https://v3.nuxtjs.org/"
-            target="_blank"
+        <span class="text-center italic"
+          >generated using
+          <a class="link link-primary" href="https://creator.nightcafe.studio"
+            >NightCafe</a
           >
-            <span
-              class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
+          with my image as source
+        </span>
+      </div>
+
+      <section class="font-serif prose-lg mt-6">
+        <h2>
+          I'm <span class="text-primary">Hardeep Kumar</span>, a 22 years old
+          Web Developer. Nice to meet you:
+        </h2>
+
+        <h3>My stacks looks as follow.</h3>
+        <ul class="font-bold">
+          <li>
+            I develop Full-Stack using
+            <span class="bg-secondary text-secondary-content px-1 rounded"
+              >Django</span
+            >.
+          </li>
+          <li>
+            I develop APIs with
+            <span class="bg-secondary text-secondary-content px-1 rounded"
+              >Django REST Framework</span
             >
-              Nuxt JS RC 13
-            </span>
-            <v-icon name="oi-link-external" scale="1.2" class="text-warning" />
-          </a>
+            and
+            <span class="bg-accent text-accent-content px-1 rounded"
+              >FastAPI</span
+            >.
+          </li>
+          <li>
+            Vue I develop frontend using
+            <span class="bg-secondary text-secondary-content px-1 rounded"
+              >Vue</span
+            >
+            and
+            <span class="bg-accent text-accent-content px-1 rounded"
+              >Nuxt JS</span
+            >. Both version 2 and 3.
+          </li>
+          <li>
+            I also develop Full stack with
+            <span class="bg-secondary text-secondary-content px-1 rounded"
+              >Django REST Framework</span
+            >
+            and
+            <span class="bg-accent text-accent-content px-1 rounded"
+              >Vue, Nuxt</span
+            >. And also with
+            <span class="bg-secondary text-secondary-content px-1 rounded"
+              >FastAPI</span
+            >
+            and
+            <span class="bg-accent text-accent-content px-1 rounded"
+              >Vue, Nuxt</span
+            >.
+          </li>
+        </ul>
+        <h3>
+          As you can see, I deal primarily with
+          <span class="bg-primary text-primary-content px-1 rounded"
+            >Python</span
+          >
+          and
+          <span class="bg-primary text-primary-content px-1 rounded"
+            >JavaScript</span
+          >. But wait, I know
+          <span class="bg-primary text-primary-content px-1 rounded"
+            >TypeScript</span
+          >
+          as well and prefer to use it over JavaScript when possible.
+        </h3>
+        <h3>
+          Im also skilled at using
+          <span class="bg-primary text-primary-content px-1 rounded">
+            Git, GitHub, CI/CD and writing tests using Django's inbuilt Testing
+            Framework </span
+          >. Aside fron this, I'm very familiar with PaaS'
+          <span class="bg-primary text-primary-content px-1 rounded">
+            Railway, Heroku and Vercel</span
+          >. And ofcourse I've my fair share with
+          <span class="bg-primary text-primary-content px-1 rounded">AWS</span>
+          as well.
+        </h3>
+        <h3>
+          Oh did i mention I use
+          <span class="bg-primary text-primary-content px-1 rounded shadow"
+            >Garuda Linux (Arch)</span
+          >?
+        </h3>
+      </section>
+
+      <h2 class="text-warning mb-4">Hobbies</h2>
+      <ul class="font-serif prose-lg font-bold mb-4">
+        <li>
+          Gaming, I play
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Genshin Impact</span
+          >
+          (no, I'm not that type of person),
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Honkai Impact</span
+          >
+          and
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Arknights</span
+          >
+          on and off.
         </li>
-
-        <li class="text-warning">
-          <a
-            class="text-warning inline-flex items-center gap-1 no-underline my-0"
-            href="https://daisyui.com"
-            target="_blank"
+        <li>
+          I like to listen to wide variety of Music from genres like
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Chillstep</span
+          >,
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Melodic Dubstep</span
+          >,
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Orchestral</span
           >
-            <span
-              class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-            >
-              Daisy UI
-            </span>
-            <v-icon name="oi-link-external" scale="1.2" class="text-warning" />
-          </a>
-          (Yes
-          <a
-            class="text-warning inline-flex items-center gap-1 no-underline my-0"
-            href="https://tailwindcss.com"
-            target="_blank"
-          >
-            <span
-              class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-            >
-              TailwindCSS
-            </span>
-            <v-icon
-              name="oi-link-external"
-              scale="1.2"
-              class="text-warning"
-            /> </a
-          >)
+          and
+          <span class="bg-secondary text-secondary-content px-1 rounded"
+            >Epic</span
+          >.
         </li>
-
-        <li class="text-warning">
-          <a
-            class="text-warning inline-flex items-center gap-1 no-underline my-0"
-            href="https://oh-vue-icons.js.org"
-            target="_blank"
-          >
-            <span
-              class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-            >
-              Oh Vue Icons
-            </span>
-            <v-icon name="oi-link-external" scale="1.2" class="text-warning" />
-          </a>
-        </li>
-
-        <li class="text-warning">
-          <a
-            class="text-warning inline-flex items-center gap-1 no-underline my-0"
-            href="https://github.com/mattboldt/typed.js/"
-            target="_blank"
-          >
-            <span
-              class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-            >
-              Typed JS
-            </span>
-            <v-icon name="oi-link-external" scale="1.2" class="text-warning" />
-          </a>
+        <li>
+          I also like to browse through various Technology related Arcticles.
         </li>
       </ul>
 
-      <p class="font-serif text-2xl">
-        The fonts used for the site are
-        <a
-          class="text-warning inline-flex items-center gap-1 no-underline my-0"
-          href="https://fonts.google.com/specimen/Nanum+Pen+Script"
-          target="_blank"
-        >
-          <span
-            class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-          >
-            Nanum Pen Script
-          </span>
-          <v-icon name="oi-link-external" scale="1.2" class="text-warning" />
-        </a>
-        and
-        <a
-          class="text-warning inline-flex items-center gap-1 no-underline my-0"
-          href="https://fonts.google.com/specimen/Mansalva"
-          target="_blank"
-        >
-          <span
-            class="no-underline after:content-[''] after:block after:w-0 after:h-[0.06rem] after:bg-warning after:transition-all after:duration-300 after:ease-in-out hover:text-warning hover:after:w-full hover:after:bg-warning"
-          >
-            Mansalva
-          </span>
-          <v-icon
-            name="oi-link-external"
-            scale="1.2"
-            class="text-warning"
-          /> </a
-        >. <br />
-        The language being used to write code for the Site is
-        <span class="text-primary">TypeScript</span>. Though I'm not that good
-        at TypeScript and just started learning it, So there might be a few
-        hiccups here and there.
-      </p>
-
-      <p class="font-serif text-2xl">
-        This Site uses two themes, namely Dracula (for dark mode) and Pastel
-        (for light mode). These themes can be toggled at will. Oh and the
-        functionality to toggle between themes are written by me since existing
-        solutions didnt pleased me.
-      </p>
-
-      <h3>Wanna Chat a bit?</h3>
-
+      <h2 class="text-primary">
+        I'm not a type of person to indulge much in Conversations but I welcome
+        them.
+      </h2>
       <HomeHeaderSocials />
-
-      <p class="font-serif text-2xl">
-        Hmm? wondering where the contact form is? nah its {{ currentYear() }}.
-        There's no need to contact via Email just dm and all good.
-      </p>
-
-      <h3>
-        You might be cursing the Attrocity of these fonts used. I understand.
-        It's ok. Well the reason to use Handwriting Fonts is Intentional. You
-        know.. to assert Supremacy.
-      </h3>
+      <br />
     </section>
   </main>
 </template>
