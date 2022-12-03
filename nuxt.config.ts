0bcf43b88ee7bf5c50e1ff3aa2e2ddaacdf9b9df
wrench1815 <hardeepkumar1815@gmail.com>
2022-12-04 00:22:30 +0530

    📦 NEW: add Nuxt Image add a test page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,12 @@ export default defineNuxtConfig({
     transpile: ['oh-vue-icons'],
   },
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxt/content'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/content',
+    '@nuxt/image-edge',
+  ],
 
   // tailwind
   tailwindcss: {},
@@ -30,6 +35,13 @@ export default defineNuxtConfig({
 
   // @nuxt/content: https://content.nuxtjs.org/api/configuration
   content: {},
+
+  // @nuxt/image-edge: https://v1.image.nuxtjs.org/get-started
+  image: {
+    cloudinary: {
+      baseURL: `https://res.cloudinary.com/${process.env.NUXT_CLOUDINARY_CLOUD_NAME}/image/upload/`,
+    },
+  },
 
   vite: {
     optimizeDeps: {
