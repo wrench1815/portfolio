5e5c4916df36e12558e7f63bd17aeddc67f52af3
wrench1815 <hardeepkumar1815@gmail.com>
2023-01-08 23:31:52 +0530

    👌 IMPROVE: add meta tags for project

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -5,6 +5,54 @@ const query: QueryBuilderParams = {
   path: '/project/',
   sort: [{ _id: -1, $numeric: true }],
 };
+
+useHead({
+  title: 'Project',
+
+  meta: [
+    {
+      name: 'description',
+      content: "Various Projects that I've worked on over time.",
+    },
+    {
+      name: 'keywords',
+      content:
+        'Hardeep Kumar, wrench1815, portfolio, Hardeep, website, Web Developer, Nuxt Developer, Vue JS Developer, Django Developer, FastAPI Developer',
+    },
+
+    // open graph
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://hardeepkumar.in' },
+    { property: 'og:title', content: 'Hardeep Kumar' },
+    {
+      property: 'og:description',
+      content: "Various Projects that I've worked on over time.",
+    },
+    {
+      property: 'og:image',
+      content:
+        'https://res.cloudinary.com/dnzbu6wqv/image/upload/v1669702340/myself/94FKLoemnSP7ikhmOunf--3--2fao5_4x_at5csh.jpg',
+    },
+    {
+      property: 'og:image:alt',
+      content: 'Hardeep Kumar AI generated art image',
+    },
+
+    // twitter
+    { property: 'twitter:card', content: 'summary_large_image' },
+    { property: 'twitter:url', content: 'https://hardeepkumar.in' },
+    { property: 'twitter:title', content: 'Hardeep Kumar' },
+    {
+      property: 'twitter:description',
+      content: "Various Projects that I've worked on over time.",
+    },
+    {
+      property: 'twitter:image',
+      content:
+        'https://res.cloudinary.com/dnzbu6wqv/image/upload/v1669702340/myself/94FKLoemnSP7ikhmOunf--3--2fao5_4x_at5csh.jpg',
+    },
+  ],
+});
 </script>
 
 <template>
