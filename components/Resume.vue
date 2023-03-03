dbebe85405ad7337aa34e44d6fde6c8ef794400a
wrench1815 <hardeepkumar1815@gmail.com>
2023-03-03 11:28:53 +0530

    👌 IMPROVE: update resume pdf link

--- components/Resume.vue
+++ components/Resume.vue
@@ -4,7 +4,7 @@
   <div class="flex justify-end">
     <a
       class="btn btn-ghost flex gap-1 items-center mb-8 text-primary hover:bg-primary hover:text-primary-content after:content-none"
-      href="https://res.cloudinary.com/dnzbu6wqv/image/upload/v1677777121/resume/hardeep_kumar_resume_aq8fus.pdf"
+      href="https://res.cloudinary.com/dnzbu6wqv/image/upload/v1677823100/resume/hardeep_kumar_resume_izxfwi.pdf"
       download="hardeep_kumar_resume_aq8fus"
       target="_blank"
     >
